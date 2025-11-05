65d2827443f7c12d59e39387426155f0e44f0a2d
GSuarato <gsuarato@Host-015.homenet.telecomitalia.it>
2025-11-05 15:55:40 +0100

    change use env var

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   plugins: [adminClient()],
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export const { signIn, signOut, useSession, signUp } = authClient;
