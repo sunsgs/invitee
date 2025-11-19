564225def67705cc17c07ff39a7ba172c410573a
GSuarato <gsuarato@Host-015.homenet.telecomitalia.it>
2025-11-19 10:45:53 +0100

    change env var for turso vercel

--- db/index.ts
+++ db/index.ts
@@ -6,7 +6,7 @@ config({ path: ".env" }); // or .env.local
 
 export const db = drizzle({
   connection: {
-    url: process.env.TURSO_CONNECTION_URL!,
+    url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
   schema: schema,
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   out: "./migrations",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
+    url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
